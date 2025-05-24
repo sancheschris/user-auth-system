19e2bf9edcc7ed69c1e5c9c01f5319f392ed08b4
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-05-24 13:31:08 -0300

    wrapping up the authHandler

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,17 +1,45 @@
 package auth
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/sancheschris/user-auth-system/internal/model"
 )
 
-type Handler struct {
+type AuthHandler struct {
 	AuthService AuthService
 }
 
-func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
+func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
+	var user model.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}	
 
+	if err := h.AuthService.Register(user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("User registered successfully"))
 }
 
-func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
-	// lógica de login
+func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+	var user model.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+	return
+	}
+
+	token, err := h.AuthService.Login(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
 }
