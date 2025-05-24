78f06a80df0d880e8a7879c7e929d37b75e34922
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-05-24 14:21:16 -0300

    add the findByUserName, SaveUser and the NewMongoUserRepository implementation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"log"
+	"net/http"
 
 	"github.com/sancheschris/user-auth-system/internal/database"
 )
@@ -10,9 +12,12 @@ func main() {
 	db, err := database.ConnectMongoDB("mongodb://root:secret@localhost:27017/?authSource=admin")
 
 	if err != nil {
-		panic(err)
+		log.Fatal("Failed to connect to MongoDB:", err)
 	}
 	defer db.Client().Disconnect(context.Background())
-	
-	db.Client()
+
+	log.Println("Server started on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
 }
--- internal/auth/repository.go
+++ internal/auth/repository.go
@@ -1,8 +1,48 @@
 package auth
 
-import "github.com/sancheschris/user-auth-system/internal/model"
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/sancheschris/user-auth-system/internal/model"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type UserRepository interface {
 	FindByUsername(username string) (*model.User, error)
 	SaveUser(user *model.User) error
+}
+
+type MongoUserRepository struct {
+	collection *mongo.Collection
+}
+
+func NewMongoUserRepository(db *mongo.Database) *MongoUserRepository {
+	return &MongoUserRepository{
+		collection: db.Collection("users"),
+	}
+}
+
+func (r *MongoUserRepository) FindByUsername(username string) (*model.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var user model.User
+	err := r.collection.FindOne(ctx, model.User{Username: username}).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *MongoUserRepository) SaveUser(user *model.User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := r.collection.InsertOne(ctx, user)
+	if mongo.IsDuplicateKeyError(err) {
+		return errors.New("user already exists")
+	}
+	return err
 }
