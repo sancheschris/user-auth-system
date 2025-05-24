1255a2eca86d3e9eb11facf2afeeee3b9a3be6ac
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-05-23 22:24:34 -0300

    add login implementation on service

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/sancheschris/user-auth-system/internal/model"
+	"github.com/sancheschris/user-auth-system/pkg/jwt"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -35,8 +36,30 @@ func (s *AuthService) Register(user model.User) error {
 	return s.Repo.SaveUser(newUser)
 }
 
+func (s *AuthService) Login(user model.User) (string, error) {
+	existingUser, err := s.Repo.FindByUsername(user.Username)
+	if err != nil {
+		return "", fmt.Errorf("user not found")
+	}
+	if !checkPassword(user.Password, existingUser.Password) {
+		return "", fmt.Errorf("invalid password")
+	}
+
+	token, err := jwt.GenerateToken(existingUser.Username)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate token")
+	}
+
+	return token, nil
+}
+
 
 func hashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err
+}
+
+func checkPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
